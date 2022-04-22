0c24e90813286ac70a0c065469bd737b94166df2
Moe <moe@chocola.dev>
2022-04-23 03:35:55 +0700

    Add workspace.sh (Linux/Mac)

---
+++ workspace.sh
@@ -0,0 +1,7 @@
+echo "=== WORKSPACE SCRIPT RUNNING ==="
+mkdir nekoya_flutter
+cd nekoya_flutter
+git clone https://gitlab.com/nekoya/app.git .
+flutter create .
+flutter pub get
+echo "=== DONE :3 ==="
