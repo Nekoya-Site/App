7a169559ac211465a3e2c6c11801cee993dc3179
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-24 17:36:57 +0700

    Added an empty Register screen

---
+++ lib/screens/register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  Register({Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
