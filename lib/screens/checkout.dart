0b04adc430f89b03d8267b08863ac9cd6445072b
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-24 17:37:32 +0700

    Added an empty Checkout screen

---
+++ lib/screens/checkout.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Checkout extends StatefulWidget {
+  Checkout({Key? key}) : super(key: key);
+
+  @override
+  State<Checkout> createState() => _CheckoutState();
+}
+
+class _CheckoutState extends State<Checkout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
