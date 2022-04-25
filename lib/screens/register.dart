b3759256d53cc8a40b8b50733b6088c963ec8729
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-25 20:49:37 +0700

    register - Linked the register's page body to the register_form

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nekoya_flutter/components/register_form.dart';
+import 'package:nekoya_flutter/components/register_verify.dart';
+import 'package:nekoya_flutter/components/register_error.dart';
 
 class Register extends StatefulWidget {
   Register({Key? key}) : super(key: key);
@@ -11,13 +14,12 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xff1b1c1e),
-      appBar: AppBar(
-        title: const Text('Register'),
-        centerTitle: true,
-        backgroundColor: const Color(0xff212226),
-      ),
-      body: Text('Register'),
-    );
+        backgroundColor: const Color(0xff1b1c1e),
+        appBar: AppBar(
+          title: const Text('Register'),
+          centerTitle: true,
+          backgroundColor: const Color(0xff212226),
+        ),
+        body: Register_Form());
   }
 }
