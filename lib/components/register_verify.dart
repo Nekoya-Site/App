4139d45503742a0ab13cea85570fe3bee1cc4bbe
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-25 20:33:18 +0700

    register_verify - Made a basic stateful widget

--- lib/components/register_verify.dart
+++ lib/components/register_verify.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class Register_Verify extends StatefulWidget {
+  Register_Verify({Key? key}) : super(key: key);
+
+  @override
+  State<Register_Verify> createState() => _Register_VerifyState();
+}
+
+class _Register_VerifyState extends State<Register_Verify> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
