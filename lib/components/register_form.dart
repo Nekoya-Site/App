f951855b6a1e8b86053a45d9f8e546d48f551632
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-25 20:39:40 +0700

    register_form - made some texts and button to use

--- lib/components/register_form.dart
+++ lib/components/register_form.dart
@@ -10,6 +10,71 @@ class Register_Form extends StatefulWidget {
 class _Register_FormState extends State<Register_Form> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(10, 10, 10, 5),
+        child: Card(
+          color: const Color(0xff212226),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: SingleChildScrollView(
+            child: Container(
+              transform: Matrix4.translationValues(0, -35, 0),
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              height: MediaQuery.of(context).size.height,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 35),
+                        child: Container(
+                          padding: const EdgeInsets.only(top: 3, left: 3),
+                          child: MaterialButton(
+                            minWidth: double.infinity,
+                            height: 35,
+                            onPressed: () {},
+                            color: const Color(0xff8B0000),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40)),
+                            child: const Text(
+                              "Register",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "Already have an account?? \nClick here to Sign In !!",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
