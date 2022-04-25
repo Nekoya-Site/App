91442312b7261b9b68cf8e64ab67f740e7092f9e
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-25 20:24:26 +0700

    Added RegisterPost in API

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -12,3 +12,22 @@ Future<dynamic> getProduct(id) async {
   var res = req.data;
   return res;
 }
+
+Future<dynamic> RegisterPost({email, password, first_name, last_name}) async {
+  Response req = await Dio().post(
+    (host + '/register'),
+    data: {
+      'email': email,
+      'password': password,
+      'first_name': first_name,
+      'last_name': last_name
+    },
+    options: Options(
+      contentType: Headers.formUrlEncodedContentType,
+      validateStatus: (status) {
+        return status! < 500;
+      },
+    ),
+  );
+  return req.statusCode;
+}
