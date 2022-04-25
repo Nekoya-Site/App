f96cb308d4761a3f3c49cc48dd3cf3879e4ca6f9
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-25 20:36:52 +0700

    register_form - created a default stateless widget

--- lib/components/register_form.dart
+++ lib/components/register_form.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class Register_Form extends StatefulWidget {
+  Register_Form({Key? key}) : super(key: key);
+
+  @override
+  State<Register_Form> createState() => _Register_FormState();
+}
+
+class _Register_FormState extends State<Register_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
