8fe787a726ab86b43921124f437520d3fb84c6d1
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-25 20:28:12 +0700

    register_error - Created basic warnings

--- lib/components/register_error.dart
+++ lib/components/register_error.dart
@@ -10,6 +10,59 @@ class Register_Error extends StatefulWidget {
 class _Register_ErrorState extends State<Register_Error> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(10, 10, 10, 5),
+        child: Card(
+          color: const Color(0xff212226),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              height: MediaQuery.of(context).size.height,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        "Error",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "An error has occured\nMake sure to check\nif your email has been registered before.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
