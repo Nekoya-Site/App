664e9467ee3b6a12847d1a7718519b8d09c8ac12
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-25 20:25:59 +0700

    Register_Error - added empty stateful widget

---
+++ lib/components/register_error.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Register_Error extends StatefulWidget {
+  Register_Error({Key? key}) : super(key: key);
+
+  @override
+  State<Register_Error> createState() => _Register_ErrorState();
+}
+
+class _Register_ErrorState extends State<Register_Error> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
