de612bc1c68fd282827cf211612cfe812688691d
Moe <moe@chocola.dev>
2022-04-26 10:44:42 +0700

    Add removeFromCart function

--- lib/data/cart.dart
+++ lib/data/cart.dart
@@ -18,3 +18,24 @@ Future<void> addToCart(productId) async {
   }
   await prefs.setString('cart', jsonEncode(cart).toString());
 }
+
+Future<void> removeFromCart(productId, bool batch) async {
+  final prefs = await SharedPreferences.getInstance();
+
+  var cart = jsonDecode(prefs.getString('cart') ?? '[]');
+  var filteredCart = cart.where((x) => x["product_id"] == productId).toList();
+  if (filteredCart.length > 0) {
+    if (batch) {
+      cart = cart.where((x) => x["product_id"] != productId).toList();
+    } else {
+      filteredCart[0]["quantity"]--;
+      if (filteredCart[0]["quantity"] == 0) {
+        cart = cart.where((x) => x["product_id"] != productId).toList();
+      } else {
+        cart = cart.where((x) => x["product_id"] != productId).toList();
+        cart.add(filteredCart[0]);
+      }
+    }
+  }
+  await prefs.setString('cart', jsonEncode(cart).toString());
+}
