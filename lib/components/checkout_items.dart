7fefe9943172a193e754159d51a1f57be2e09e09
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-28 14:20:01 +0700

    checkout_items - Added a card with list of items in cart

--- lib/components/checkout_items.dart
+++ lib/components/checkout_items.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nekoya_flutter/api/api.dart';
+import 'package:nekoya_flutter/data/cart.dart';
+import 'package:nekoya_flutter/components/cart_box.dart';
 
 class CheckoutItems extends StatefulWidget {
   CheckoutItems({Key? key}) : super(key: key);
@@ -8,8 +11,85 @@ class CheckoutItems extends StatefulWidget {
 }
 
 class _CheckoutItemsState extends State<CheckoutItems> {
+  Future<dynamic> _viewCart = viewCart();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.fromLTRB(10, 10, 10, 5),
+      child: Card(
+        color: const Color(0xff212226),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const Center(
+              child: Text(
+                "Items in Cart",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),
+              ),
+            ),
+            FutureBuilder<dynamic>(
+              future: _viewCart,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  var data = snapshot.data;
+                  return ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: data!.length,
+                    itemBuilder: (context, index) {
+                      return FutureBuilder<dynamic>(
+                        future: getProduct(data[index]["product_id"]),
+                        builder: (context, snapshotx) {
+                          if (snapshotx.hasData) {
+                            var productData = snapshotx.data;
+                            if (productData != null) {
+                              return CartBox(
+                                  controller: false,
+                                  imageUrl: 'https://nekoya.moe.team/img/' +
+                                      productData[0]['IMAGE'],
+                                  title: productData[0]['TITLE'],
+                                  quantity: data[index]["quantity"],
+                                  plus: () {},
+                                  minus: () {},
+                                  remove: () {});
+                            }
+                          }
+
+                          return CartBox(
+                            controller: false,
+                            imageUrl:
+                                'https://i.ibb.co/QJFLZC4/La-Darknesss-Portrait.webp',
+                            title: 'Loading...',
+                            quantity: 0,
+                            plus: () {},
+                            minus: () {},
+                            remove: () {},
+                          );
+                        },
+                      );
+                    },
+                  );
+                }
+
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: Color(0xff8B0000),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
