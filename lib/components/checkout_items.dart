55ee837fbe1d0f84bb4d2f9e7f10d08000d84e41
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-28 13:56:12 +0700

    checkout_items - added stateful widget template

---
+++ lib/components/checkout_items.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CheckoutItems extends StatefulWidget {
+  CheckoutItems({Key? key}) : super(key: key);
+
+  @override
+  State<CheckoutItems> createState() => _CheckoutItemsState();
+}
+
+class _CheckoutItemsState extends State<CheckoutItems> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
