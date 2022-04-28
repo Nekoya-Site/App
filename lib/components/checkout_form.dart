6b87362ae09d477e242278fc439f25a0c1381020
Matthew Patrick <Matthew.535200018@stu.untar.ac.id>
2022-04-28 09:12:46 +0700

    checkout_form - Added an empty widget

--- lib/components/checkout_form.dart
+++ lib/components/checkout_form.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:nekoya_flutter/api/api.dart';
 
+class CheckoutForm extends StatefulWidget {
+  CheckoutForm({Key? key}) : super(key: key);
+
+  @override
+  State<CheckoutForm> createState() => _CheckoutFormState();
+}
+
+class _CheckoutFormState extends State<CheckoutForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
