42ff3c73a3608ad09c60b63b7b01f986013f0035
D Shrat <dshrat29@gmail.com>
2022-04-29 14:11:05 +0700

    create otp file

---
+++ lib/components/otp_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OtpBody extends StatefulWidget {
+  const OtpBody({Key? key}) : super(key: key);
+
+  @override
+  State<OtpBody> createState() => _OtpBodyState();
+}
+
+class _OtpBodyState extends State<OtpBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/otp.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:nekoya_flutter/components/otp_body.dart';
+
+class Otp extends StatefulWidget {
+  const Otp({Key? key}) : super(key: key);
+
+  @override
+  State<Otp> createState() => _OtpState();
+}
+
+class _OtpState extends State<Otp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff1b1c1e),
+      appBar: AppBar(
+        title: const Text('Otp'),
+        centerTitle: true,
+        backgroundColor: const Color(0xff212226),
+      ),
+      body: const OtpBody(),
+    );
+  }
+}
