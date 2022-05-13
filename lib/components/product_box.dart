a49b60c4abc05e7dca6bc41d1cb71e0911b864a4
Moe <moe@chocola.dev>
2022-05-13 11:12:11 +0700

    Remove unused import

--- lib/components/product_box.dart
+++ lib/components/product_box.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:responsify/responsify.dart';
 import 'package:flutter/material.dart';
 
 class ProductBox extends StatefulWidget {
--- lib/screens/products.dart
+++ lib/screens/products.dart
@@ -1,4 +1,3 @@
-import 'package:responsify/responsify.dart';
 import 'package:flutter/material.dart';
 
 import 'package:nekoya_flutter/api/api.dart';
