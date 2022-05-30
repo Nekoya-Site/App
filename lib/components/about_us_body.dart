96b9a2e3534fafc2064b6d6b6e54b02229384520
Hansedison02 <hans.535200041@stu.untar.ac.id>
2022-05-30 11:56:52 +0700

    Fixed Scroll problems in About Us

--- lib/components/about_us_body.dart
+++ lib/components/about_us_body.dart
@@ -15,74 +15,90 @@ class _AboutUsBodyState extends State<AboutUsBody> {
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.network(
-        'https://nekoya.moe.team/img/AboutUs.mp4')
-      ..initialize().then((_) {
-        setState(() {
-          _controller.play();
-          _controller.setLooping(true);
-        });
-      });
+    _controller =
+        VideoPlayerController.network('https://nekoya.moe.team/img/AboutUs.mp4')
+          ..initialize().then((_) {
+            setState(() {
+              _controller.play();
+              _controller.setLooping(true);
+            });
+          });
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
         children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-            child: _controller.value.isInitialized
-            ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              )
-            : const Image(image: AssetImage('assets/images/about_us_thumbnail.webp'),),
-          ),
-          Card(
-            color: const Color(0xff212226),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Nekoya is your place for sneakers for the entire family from many name brands. You’ll discover styles for ladies, men and children from brands like Nike, Converse, Vans, Sperry, Madden Girl, Skechers, ASICS and then some! Nekoya is a main family footwear goal for the popular brands you know and love.',
-                style: TextStyle(fontSize: 20.0, color: Colors.white, height: 1.5),
-                textAlign: TextAlign.justify,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                child: _controller.value.isInitialized
+                    ? AspectRatio(
+                        aspectRatio: _controller.value.aspectRatio,
+                        child: VideoPlayer(_controller),
+                      )
+                    : const Image(
+                        image:
+                            AssetImage('assets/images/about_us_thumbnail.webp'),
+                      ),
               ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(bottom: 10.0),
-            child: ElevatedButton(
-              style: ButtonStyle(
-                padding: MaterialStateProperty.all(const EdgeInsets.all(15.0)),
-                foregroundColor:
-                    MaterialStateProperty.all(const Color(0xff8B0000)),
-                backgroundColor:
-                    MaterialStateProperty.all(const Color(0xff8B0000)),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: const BorderSide(color: Colors.black)))),
-              child: const Text(
-                'Contact Us',
-                style: TextStyle(color: Colors.white, fontSize: 20),
+              Card(
+                color: const Color(0xff212226),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    'Nekoya is your place for sneakers for the entire family from many name brands. You’ll discover styles for ladies, men and children from brands like Nike, Converse, Vans, Sperry, Madden Girl, Skechers, ASICS and then some! Nekoya is a main family footwear goal for the popular brands you know and love.',
+                    style: TextStyle(
+                        fontSize: 20.0, color: Colors.white, height: 1.5),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
               ),
-              onPressed: () async {
-                if (!await launchUrl(Uri.parse('mailto:nekoya@chocola.dev'))) throw 'Could not launch';
-              },
-            ),
-          ),
-          GestureDetector(
-            onTap: () async {
-              if (!await launchUrl(Uri.parse('https://nekoya.moe.team'))) throw 'Could not launch';
-            },
-            child: const Text('© 2021-2022 Nekoya Co. Ltd.', style: TextStyle(color: Colors.white, fontSize: 15),)
+              Container(
+                margin: const EdgeInsets.only(bottom: 10.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                      padding:
+                          MaterialStateProperty.all(const EdgeInsets.all(15.0)),
+                      foregroundColor:
+                          MaterialStateProperty.all(const Color(0xff8B0000)),
+                      backgroundColor:
+                          MaterialStateProperty.all(const Color(0xff8B0000)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: const BorderSide(color: Colors.black)))),
+                  child: const Text(
+                    'Contact Us',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  onPressed: () async {
+                    if (!await launchUrl(
+                        Uri.parse('mailto:nekoya@chocola.dev')))
+                      throw 'Could not launch';
+                  },
+                ),
+              ),
+              GestureDetector(
+                  onTap: () async {
+                    if (!await launchUrl(Uri.parse('https://nekoya.moe.team')))
+                      throw 'Could not launch';
+                  },
+                  child: const Text(
+                    '© 2021-2022 Nekoya Co. Ltd.',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  )),
+            ],
           ),
         ],
       ),
